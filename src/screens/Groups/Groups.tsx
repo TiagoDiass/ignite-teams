1600a8e75f5014a94ab17332dc8ccc1c5beb00e1
TiagoDiass <tiago.costadiasss@gmail.com>
2022-12-19 13:10:25 -0300

    refactor(setup): change initial structure, add src and screens folder and Groups component

---
+++ src/screens/Groups/Groups.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { styles } from './Groups.styles';
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Groups</Text>
+    </View>
+  );
+}
